8bf5317eb8b9b95eafaab00becb230427509d630
Abduvohid <ilhomovvahid@gmail.com>
2024-09-06 15:59:58 +0200

    Added middleware and sql-database

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+/*
+* This is the main file for the server. It will be responsible for handling all the requests and responses.
+* It will also be responsible for connecting to the database.
+*/
+
+"use strict";
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 1337;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',   // Update as per your credentials
+    password: 'password',
+    database: 'ticketing_system'
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
