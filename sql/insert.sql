873c05cee29234257caf4672e5c32fd72d70e51f
Abduvohid <ilhomovvahid@gmail.com>
2024-09-21 17:17:33 +0200

    Added: a csv file with a insert statement

--- sql/insert.sql
+++ sql/insert.sql
@@ -4,7 +4,7 @@
 
 DELETE FROM attachments; -- Clear the attachments table.
 DELETE FROM tickets;
-
+DELETE FROM users;
 --
 -- Enable LOAD DATA LOCAL INFILE on the server.
 --
@@ -18,3 +18,11 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (id, title, description, department, status);
+
+LOAD DATA LOCAL INFILE 'users.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(id, username, password, google_id, email, role);
